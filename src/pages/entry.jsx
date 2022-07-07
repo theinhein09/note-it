92dbf8fc67c02a65b221a057789d1a2540d433d3
t-hein04 <108310762+t-hein04@users.noreply.github.com>
2022-07-07 01:39:15 -0700

    Update entry.jsx

--- src/pages/entry.jsx
+++ src/pages/entry.jsx
@@ -3,12 +3,11 @@ import useBoolean from "../hooks/useBoolean";
 import LoginFormContainer from "../containers/login-form-container";
 import ErrorContainer from "../containers/error-container";
 import ModalContainer from "../containers/modal-container";
-import DialogContainer from "../containers/dialog-container";
 import SignupFormContainer from "../containers/signup-form-container";
 import Loading from "../components/loading";
+import DialogRenderButtonContainer from "../containers/dialog-render-button-container";
 
 const Entry = () => {
-  const [dialog, { on: openDialog, off: closeDialog }] = useBoolean();
   const [loading, { on: startLoading, off: finishLoading }] = useBoolean();
   const [message, setMessage] = useState(null);
 
@@ -26,19 +25,17 @@ const Entry = () => {
       <p>New User for Testing</p>
       <p>Email: new_user@name.ad</p>
       <p>Password: q</p>
-      <button onClick={openDialog}>Sign Up</button>
+      <DialogRenderButtonContainer
+        buttonLabel="Sign Up"
+        render={() => (
+          <SignupFormContainer
+            startLoading={startLoading}
+            finishLoading={finishLoading}
+            setMessage={setMessage}
+          />
+        )}
+      />
       <ErrorContainer />
-      {dialog && (
-        <ModalContainer>
-          <DialogContainer closeDialog={closeDialog}>
-            <SignupFormContainer
-              startLoading={startLoading}
-              finishLoading={finishLoading}
-              setMessage={setMessage}
-            />
-          </DialogContainer>
-        </ModalContainer>
-      )}
       {loading && (
         <ModalContainer>
           <Loading />
