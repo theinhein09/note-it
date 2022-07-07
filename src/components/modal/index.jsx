e75a18e793c7a86c96a6fc5eaed817f446f79e63
t-hein04 <108310762+t-hein04@users.noreply.github.com>
2022-07-07 00:02:23 -0700

    Add styling to Modal

--- src/components/modal/index.jsx
+++ src/components/modal/index.jsx
@@ -2,7 +2,14 @@ import React from "react";
 
 const Modal = (props) => {
   const { children } = props;
-  return <div role="presentation">{children}</div>;
+  return (
+    <div
+      role="presentation"
+      className="absolute w-screen h-screen  bg-white/50 top-0 backdrop-blur-sm grid place-content-center"
+    >
+      {children}
+    </div>
+  );
 };
 
 export default Modal;
