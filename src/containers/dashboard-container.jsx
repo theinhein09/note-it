9055c83782d76c60a83ded69cb47ecb2aa4e3653
t-hein04 <108310762+t-hein04@users.noreply.github.com>
2022-07-06 19:18:27 -0700

    Create dashboard-container.jsx

---
+++ src/containers/dashboard-container.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Dashboard from "../components/dashboard";
+
+const DashboardContainer = (props) => {
+  return <Dashboard {...props} />;
+};
+
+export default DashboardContainer;
