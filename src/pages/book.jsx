3b38a9248e03b5507cc978df87f721612df8e2ff
t-hein04 <108310762+t-hein04@users.noreply.github.com>
2022-07-06 18:57:10 -0700

    Update book.jsx
    remove sidebar

--- src/pages/book.jsx
+++ src/pages/book.jsx
@@ -5,9 +5,6 @@ import useBoolean from "../hooks/useBoolean";
 import { getBook } from "../utils/fakeAPI";
 import { IoMdArrowRoundBack } from "react-icons/io";
 import { groupBy } from "lodash";
-import SidebarContainer from "../containers/sidebar-container";
-import { FaChevronRight } from "react-icons/fa";
-import SectionsContainer from "../containers/sections-container";
 import TextEditorContainer from "../containers/text-editor-container";
 import LayoutContainer from "../containers/layout-container";
 
@@ -15,10 +12,6 @@ const Book = () => {
   const { userId, bookId } = useParams();
   const [sections, setSections] = useState(null);
   const [loading, { on: startLoading, off: finishLoading }] = useBoolean(true);
-  const [
-    sidebar,
-    { toggle: toggleSidebar, on: openSidebar, off: closeSidebar },
-  ] = useBoolean();
 
   const navigate = useNavigate();
 
@@ -38,21 +31,7 @@ const Book = () => {
       <button onClick={() => navigate(`/${userId}`)}>
         <IoMdArrowRoundBack />
       </button>
-      <button onClick={toggleSidebar}>
-        <FaChevronRight />
-      </button>
-      {loading ? (
-        <Loading />
-      ) : (
-        <>
-          {sidebar && (
-            <SidebarContainer closeSidebar={closeSidebar}>
-              <SectionsContainer sections={sections} />
-            </SidebarContainer>
-          )}
-          <TextEditorContainer />
-        </>
-      )}
+      {loading ? <Loading /> : <TextEditorContainer />}
     </LayoutContainer>
   );
 };
