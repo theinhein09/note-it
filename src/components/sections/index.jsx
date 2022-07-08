b93576614bd9ab946d6591ccf64739802e90d4ad
t-hein04 <108310762+t-hein04@users.noreply.github.com>
2022-07-07 18:09:31 -0700

    Update index.jsx
    sections component

--- src/components/sections/index.jsx
+++ src/components/sections/index.jsx
@@ -1,6 +1,6 @@
 import React, { Fragment } from "react";
 import ButtonContainer from "../../containers/button-container";
-import { FaEdit } from "react-icons/fa";
+import { FaEdit, FaRegEye } from "react-icons/fa";
 import { RiDeleteBin6Line } from "react-icons/ri";
 
 const Sections = (props) => {
@@ -10,7 +10,7 @@ const Sections = (props) => {
     let pages = [];
     for (let section in sections) {
       pages.push(
-        <section className="bg-slate-600 font-display" key={section}>
+        <section className="font-display" key={section}>
           <header className="flex items-center bg-black text-white">
             <h3 className="grow px-1">{section}</h3>
             <ButtonContainer
@@ -23,18 +23,25 @@ const Sections = (props) => {
               icon={<RiDeleteBin6Line />}
               category="icon-only"
             />
+            <ButtonContainer label="Add New Page" className="text-sm" />
           </header>
           {sections[section].map((page) => (
             <article
               onClick={openPage(page)}
               key={page.id}
-              className="flex items-center"
+              className="flex cursor-pointer items-center"
             >
-              <div role="presentation" className="ml-4 h-4 w-0.5 bg-black" />
-              <div role="presentation" className="h-0.5 w-4 bg-black" />
+              <div role="presentation" className="ml-4 h-5 w-0.5 bg-black" />
+              <div role="presentation" className="h-0.5 w-2 bg-black" />
               <h4 className="grow overflow-hidden text-ellipsis px-2">
                 {page.title}
               </h4>
+              <ButtonContainer
+                icon={<FaRegEye />}
+                onClick={openPage(page)}
+                category="icon-only"
+                title="Open Page in Text Editor"
+              />
               <ButtonContainer
                 onClick={() => console.log("EDIT Page Title")}
                 icon={<FaEdit />}
@@ -47,7 +54,6 @@ const Sections = (props) => {
               />
             </article>
           ))}
-          <ButtonContainer label="Add Page" />
         </section>
       );
     }
