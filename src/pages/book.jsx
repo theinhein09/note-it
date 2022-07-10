f9a2a70a467b3eb1100a0cfcdd871d58d353eda7
t-hein04 <108310762+t-hein04@users.noreply.github.com>
2022-07-10 04:34:03 -0700

    Add save new page

--- src/pages/book.jsx
+++ src/pages/book.jsx
@@ -9,6 +9,10 @@ import ButtonContainer from "../containers/button-container";
 import { useUserContextState } from "../contexts/user-context";
 import FireStore from "../firebase/firestore";
 import useOnSnapshot from "../hooks/useOnSnapshot";
+import ModalContainer from "../containers/modal-container";
+import DialogContainer from "../containers/dialog-container";
+import InputContainer from "../containers/input-container";
+import useBoolean from "../hooks/useBoolean";
 
 const Book = () => {
   const { bookId } = useParams();
@@ -17,6 +21,11 @@ const Book = () => {
   const [selectedPage, setSelectedPage] = useState(null);
   const [selectedBook, setSelectedBook] = useState(null);
   const [currentContent, setCurrentContent] = useState();
+  const [page, setPage] = useState({
+    title: "",
+    section: "",
+  });
+  const [dialog, { on: openDialog, off: closeDialog }] = useBoolean();
 
   const navigate = useNavigate();
 
@@ -41,50 +50,93 @@ const Book = () => {
     setSections(groupBySection);
   }, [pages]);
 
-  const savePage = () => {
-    console.log(currentContent);
+  const handleSave = () => {
+    const content = JSON.stringify(currentContent);
+    const data = { content, ...page };
+    const pagesFS = new FireStore(`users/${user.uid}/books/${bookId}/pages`);
+    pagesFS.addDoc(data);
+    closeDialog();
+  };
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setPage((prev) => ({ ...prev, [name]: value }));
   };
 
   return (
-    <LayoutContainer
-      sections={sections}
-      setSelectedPage={selectedPageMemo.setSelectedPage}
-    >
-      <nav className="ml-8 flex max-h-8 items-center bg-black px-1 font-display text-white">
-        <div role="presentation" className="flex grow divide-x-2">
-          {!user ? (
-            <Loading />
-          ) : (
-            <span className="px-4">{user.displayName}</span>
+    <>
+      <LayoutContainer
+        sections={sections}
+        setSelectedPage={selectedPageMemo.setSelectedPage}
+      >
+        <nav className="ml-8 flex max-h-8 items-center bg-black px-1 font-display text-white">
+          <div role="presentation" className="flex grow divide-x-2">
+            {!user ? (
+              <Loading />
+            ) : (
+              <span className="px-4">{user.displayName}</span>
+            )}
+            {!selectedBook ? (
+              <Loading />
+            ) : (
+              <>
+                <span className="px-4">Category: {selectedBook.category}</span>
+                <span className="px-4">Title: {selectedBook.title}</span>
+              </>
+            )}
+            {selectedPage && (
+              <>
+                <span className="px-4">Section: {selectedPage.section}</span>
+                <span className="px-4">Page: {selectedPage.title}</span>
+              </>
+            )}
+          </div>
+          {currentContent && (
+            <ButtonContainer icon="Save" onClick={() => openDialog()} />
           )}
-          {!selectedBook ? (
-            <Loading />
-          ) : (
-            <>
-              <span className="px-4">Category: {selectedBook.category}</span>
-              <span className="px-4">Title: {selectedBook.title}</span>
-            </>
-          )}
-          {selectedPage && (
-            <>
-              <span className="px-4">Section: {selectedPage.section}</span>
-              <span className="px-4">Page: {selectedPage.title}</span>
-            </>
-          )}
-        </div>
-        {currentContent && <ButtonContainer icon="Save" onClick={savePage} />}
-        <ButtonContainer
-          icon={<VscChromeClose title="close book" />}
-          category="icon-only"
-          onClick={() => navigate("/")}
-          className="text-white"
+          <ButtonContainer
+            icon={<VscChromeClose title="close book" />}
+            category="icon-only"
+            onClick={() => navigate("/")}
+            className="text-white"
+          />
+        </nav>
+        <TextEditorContainer
+          selectedPage={selectedPageMemo.selectedPage}
+          setCurrentContent={setCurrentContent}
         />
-      </nav>
-      <TextEditorContainer
-        selectedPage={selectedPageMemo.selectedPage}
-        setCurrentContent={setCurrentContent}
-      />
-    </LayoutContainer>
+      </LayoutContainer>
+      {dialog && (
+        <ModalContainer>
+          <DialogContainer hideButton={true}>
+            <div role="presentation" className="bg-white font-display">
+              <p className="bg-black p-1 text-white">Save Page</p>
+              <div
+                role="presentation"
+                className="flex flex-col gap-2 px-1 py-3"
+              >
+                <InputContainer
+                  label="Title"
+                  value={page.title}
+                  name="title"
+                  onChange={handleInputChange}
+                />
+                <InputContainer
+                  label="Section"
+                  value={page.section}
+                  name="section"
+                  onChange={handleInputChange}
+                />
+              </div>
+              <div role="presentation" className="flex justify-between p-1">
+                <ButtonContainer label="Save" onClick={handleSave} />
+                <ButtonContainer label="Cancel" onClick={() => closeDialog()} />
+              </div>
+            </div>
+          </DialogContainer>
+        </ModalContainer>
+      )}
+    </>
   );
 };
 
