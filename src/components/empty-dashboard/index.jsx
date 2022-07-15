68ba557c1e0230ea3002860961737253b168683c
theinhein09 <theinhein09@gmail.com>
2022-07-14 23:00:18 -0700

    Modified empty dashboard

--- src/components/empty-dashboard/index.jsx
+++ src/components/empty-dashboard/index.jsx
@@ -6,10 +6,10 @@ import CreateBookDialog from "../dialog/create-book-dialog";
 const EmptyDashboard = (props) => {
   const [dialog, { on: openDialog, off: closeDialog }] = useBoolean();
   return (
-    <>
+    <div className="fixed top-1/2 left-1/2">
       <ButtonContainer label="Create Book" onClick={openDialog} />
       {dialog && <CreateBookDialog closeDialog={closeDialog} />}
-    </>
+    </div>
   );
 };
 
